381b1e95e084defde31a0cc9c006dd7514cd2019
gs <gs>
2019-10-13 17:31:51 +0800

    lklklk

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Render from './render';
+import CustomizedRatings from "./bits/rate"
+import { createMuiTheme } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+
+
 
 const App: React.FC = () => {
+  const [theme, setTheme] = useState(createMuiTheme({
+    palette: {
+      primary: blue,
+    },
+  }));
+  const [dark, setDark] = useState(false);
+
   return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        
     <div className="App">
       <div id='canvas-frame'>
         <Render />
       </div>
+      <CustomizedRatings />
+      <button
+      style={{
+        position:'absolute',
+        right: 5
+      }}
+      onClick={()=>{
+        setTheme(createMuiTheme({
+          palette: {
+            type: dark ? 'dark' : 'light',
+            primary: blue,
+          },
+        }))
+        setDark(!dark)
+      }}
+    >Click me</button>
     </div>
+    
+      </CssBaseline>
+    </ThemeProvider>
   );
 }
 
